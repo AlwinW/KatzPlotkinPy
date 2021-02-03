120e3ecc37bdfc85008a9753f141d7537bc058f9
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-02-04 09:31:50 +1100

    Afgen chord length

--- src/katzplotkinpy/afgen.py
+++ src/katzplotkinpy/afgen.py
@@ -2,10 +2,12 @@
 
 import logging
 
+import numpy as np
+
 logger = logging.getLogger(__name__)
 
 
-def afgen(e: float, k: float, alpha: float, m: int):
+def afgen(e: float, k: float, alpha: float, m: int, l: float = 1):
     """Generates van de Vooren Airfoil. Refer to Section 6.7
 
     :param e: Thickness coefficient (between 0 and 1)
@@ -16,13 +18,15 @@ def afgen(e: float, k: float, alpha: float, m: int):
     :type alpha: float
     :param m: Number of panels
     :type m: int
+    :param l: Half chord length
+    :type l: float
     """
     logger.info(
         "Starting van de Vooren Transformation with "
         + f"e = '{e}', ak = '{k}', alpha = '{alpha}', m = '{m}'"
     )
-    tl = 1.0  # l
-    a = 2 * tl * (e + 1) ** (k - 1) / (2 ** k)  # Eq (6.73) ref Eq. (6.63)
+    l = 1.0  # Chord length 2l
+    a = 2 * l * np.power(e + 1, k - 1) / np.power(2, k)  # Eq (6.73) ref Eq. (6.63)
     al = alpha / 57.2957795131
     try:
         m = int(m)
@@ -33,7 +37,3 @@ def afgen(e: float, k: float, alpha: float, m: int):
     theta = range(0, 1)
 
     print(a, al, theta)
-
-
-if __name__ == "__main__":
-    afgen(1, 1, 1, 1)
